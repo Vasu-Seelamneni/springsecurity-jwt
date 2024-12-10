49351a1288b4c72b727b8920e5fd0b18c76f29cc
Vasu Seelamneni <vasu_seelamneni@dell.com>
2024-12-10 19:41:33 +0800

    Spring Security with form based, basic and InMemory mechanisms.

---
+++ src/main/java/com/msp/security/springsecurityjwt/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.msp.security.springsecurityjwt.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return "Hello, User";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint() {
+        return "Hello, Admin";
+    }
+}
